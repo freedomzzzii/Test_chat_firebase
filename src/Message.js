bc8d82a900910ecace847a40e93c310e39e5f85a
freedomzzzii <theeraporn.tia@gmail.com>
2018-10-19 16:31:40 +0700

    chat 1 room

---
+++ src/Message.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+export default class Message extends Component {
+  render() {
+    return (
+      <div className="message">
+                <span className="message__author">
+                    {this.props.message.userName}:
+                </span>
+        {this.props.message.message}
+      </div>
+    )
+  }
+}
